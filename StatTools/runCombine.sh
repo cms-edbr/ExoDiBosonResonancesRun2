100f89db2ba7d9788bb3a9c5adc225b5fd596f8a
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-04-29 15:09:21 +0200

    Add macro to submit shape analysis

--- StatTools/runCombine.sh
+++ StatTools/runCombine.sh
@@ -8,41 +8,48 @@
 #
 # The output trees are merged and analyzed by another macro "plotLimit.C" 
 
-combine -M Asymptotic -d dataCards/EHP1000_shape_card.txt -m 1000 -n EHP
-combine -M Asymptotic -d dataCards/EHP2000_shape_card.txt -m 2000 -n EHP
-combine -M Asymptotic -d dataCards/EHP3000_shape_card.txt -m 3000 -n EHP
-combine -M Asymptotic -d dataCards/EHP4000_shape_card.txt -m 4000 -n EHP
+eval `scramv1 runtime -sh`
+
+combine -M Asymptotic -d dataCards/CMS_ZZ_1000_EHP_13TeV.txt -m 1000 -n EHP
+combine -M Asymptotic -d dataCards/CMS_ZZ_2000_EHP_13TeV.txt -m 2000 -n EHP
+combine -M Asymptotic -d dataCards/CMS_ZZ_3000_EHP_13TeV.txt -m 3000 -n EHP
+combine -M Asymptotic -d dataCards/CMS_ZZ_4000_EHP_13TeV.txt -m 4000 -n EHP
 hadd combineEHP.root higgsCombineEHP.Asymptotic.mH1000.root higgsCombineEHP.Asymptotic.mH2000.root higgsCombineEHP.Asymptotic.mH3000.root higgsCombineEHP.Asymptotic.mH4000.root
  
-combine -M Asymptotic -d dataCards/ELP1000_shape_card.txt -m 1000 -n ELP
-combine -M Asymptotic -d dataCards/ELP2000_shape_card.txt -m 2000 -n ELP
-combine -M Asymptotic -d dataCards/ELP3000_shape_card.txt -m 3000 -n ELP
-combine -M Asymptotic -d dataCards/ELP4000_shape_card.txt -m 4000 -n ELP
+combine -M Asymptotic -d dataCards/CMS_ZZ_1000_ELP_13TeV.txt -m 1000 -n ELP
+combine -M Asymptotic -d dataCards/CMS_ZZ_2000_ELP_13TeV.txt -m 2000 -n ELP
+combine -M Asymptotic -d dataCards/CMS_ZZ_3000_ELP_13TeV.txt -m 3000 -n ELP
+combine -M Asymptotic -d dataCards/CMS_ZZ_4000_ELP_13TeV.txt -m 4000 -n ELP
 hadd combineELP.root higgsCombineELP.Asymptotic.mH1000.root higgsCombineELP.Asymptotic.mH2000.root higgsCombineELP.Asymptotic.mH3000.root higgsCombineELP.Asymptotic.mH4000.root 
 
-combine -M Asymptotic -d dataCards/MHP1000_shape_card.txt -m 1000 -n MHP
-combine -M Asymptotic -d dataCards/MHP2000_shape_card.txt -m 2000 -n MHP
-combine -M Asymptotic -d dataCards/MHP3000_shape_card.txt -m 3000 -n MHP
-combine -M Asymptotic -d dataCards/MHP4000_shape_card.txt -m 4000 -n MHP
+combine -M Asymptotic -d dataCards/CMS_ZZ_1000_MHP_13TeV.txt -m 1000 -n MHP
+combine -M Asymptotic -d dataCards/CMS_ZZ_2000_MHP_13TeV.txt -m 2000 -n MHP
+combine -M Asymptotic -d dataCards/CMS_ZZ_3000_MHP_13TeV.txt -m 3000 -n MHP
+combine -M Asymptotic -d dataCards/CMS_ZZ_4000_MHP_13TeV.txt -m 4000 -n MHP
 hadd combineMHP.root higgsCombineMHP.Asymptotic.mH1000.root higgsCombineMHP.Asymptotic.mH2000.root higgsCombineMHP.Asymptotic.mH3000.root higgsCombineMHP.Asymptotic.mH4000.root
  
-combine -M Asymptotic -d dataCards/MLP1000_shape_card.txt -m 1000 -n MLP
-combine -M Asymptotic -d dataCards/MLP2000_shape_card.txt -m 2000 -n MLP
-combine -M Asymptotic -d dataCards/MLP3000_shape_card.txt -m 3000 -n MLP
-combine -M Asymptotic -d dataCards/MLP4000_shape_card.txt -m 4000 -n MLP
+combine -M Asymptotic -d dataCards/CMS_ZZ_1000_MLP_13TeV.txt -m 1000 -n MLP
+combine -M Asymptotic -d dataCards/CMS_ZZ_2000_MLP_13TeV.txt -m 2000 -n MLP
+combine -M Asymptotic -d dataCards/CMS_ZZ_3000_MLP_13TeV.txt -m 3000 -n MLP
+combine -M Asymptotic -d dataCards/CMS_ZZ_4000_MLP_13TeV.txt -m 4000 -n MLP
 hadd combineMLP.root higgsCombineMLP.Asymptotic.mH1000.root higgsCombineMLP.Asymptotic.mH2000.root higgsCombineMLP.Asymptotic.mH3000.root higgsCombineMLP.Asymptotic.mH4000.root 
 
 # ROOT6
 source /afs/cern.ch/sw/lcg/contrib/gcc/4.8/x86_64-slc6/setup.sh
-source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.03.02/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
+source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.03.04/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
 
 root -b -q 'plotLimit.C("EHP")'
 root -b -q 'plotLimit.C("ELP")'
 root -b -q 'plotLimit.C("MHP")'
 root -b -q 'plotLimit.C("MLP")'
 
+mv limitEHP.pdf limitShapeEHP.pdf
+mv limitELP.pdf limitShapeELP.pdf
+mv limitMLP.pdf limitShapeMLP.pdf
+mv limitMHP.pdf limitShapeMHP.pdf
+
 # remove useless files
 rm roostats-*
-rm *000.root
+rm *.root
 
 
