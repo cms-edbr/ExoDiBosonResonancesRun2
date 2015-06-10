505522b4c686d36ed201a2252ecf96de5d68e1fc
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-06-10 19:13:37 -0300

    Insert primary vertex filter before good leptons producer

--- EDBRLeptons/python/goodLeptonsProducer_cff.py
+++ EDBRLeptons/python/goodLeptonsProducer_cff.py
@@ -1,5 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
+goodOfflinePrimaryVertex = cms.EDFilter("VertexSelector",
+                                       src = cms.InputTag("offlineSlimmedPrimaryVertices"),
+                                       cut = cms.string("chi2!=0 && ndof >= 4.0 && abs(z) <= 24.0 && abs(position.Rho) <= 2.0"),
+                                       filter = cms.bool(True)
+                                       )
+
 goodLeptons = cms.EDProducer("goodLeptonsProducer",
                               vertex = cms.InputTag("goodOfflinePrimaryVertex"),
                               electrons = cms.InputTag("slimmedElectrons"),
@@ -8,4 +14,4 @@ goodLeptons = cms.EDProducer("goodLeptonsProducer",
                               eltightID  = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-tight")
                             )
 
-goodLeptonsProducer = cms.Sequence(goodLeptons)
+goodLeptonsProducer = cms.Sequence(goodOfflinePrimaryVertex + goodLeptons)
