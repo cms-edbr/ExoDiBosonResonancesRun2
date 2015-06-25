c795dc2a1906167d558879817ca910bf0c13bb3b
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-06-25 19:21:11 -0300

    remove extra spaces

--- EDBRGenStudies/python/selectHadronicDecay.py
+++ EDBRGenStudies/python/selectHadronicDecay.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 hadronicDecay = cms.EDFilter("CandViewSelector",
                              src = cms.InputTag("prunedGenParticles"),
-                             cut = cms.string("22 < abs(pdgId()) < 26            &&" + \
-                                              " 0 < abs(daughter(0).pdgId()) < 6 &&" + \
-                                              "mother(0).pdgId()==5100039"),
+                             cut = cms.string(" 22 < abs(pdgId()) < 26 "
+                                              "& 0 < abs(daughter(0).pdgId()) < 6 "
+                                              "& mother(0).pdgId()==5100039"),
                              filter = cms.bool(True)
                             )
--- EDBRGenStudies/python/selectLeptonicDecay.py
+++ EDBRGenStudies/python/selectLeptonicDecay.py
@@ -2,9 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 leptonicDecay = cms.EDFilter("CandViewSelector",
                              src = cms.InputTag("prunedGenParticles"),
-                             cut = cms.string("22 < abs(pdgId()) < 26                                        &&" + \
-                                              "(abs(daughter(0).pdgId())==11||abs(daughter(1).pdgId())==11||"    + \
-                                              " abs(daughter(0).pdgId())==13||abs(daughter(1).pdgId())==13 ) &&" + \
-                                              "mother(0).pdgId()==5100039"),
+                             cut = cms.string("22 < abs(pdgId()) < 26 "
+                                              "& (abs(daughter(0).pdgId())==11||abs(daughter(1).pdgId())==11|| " 
+                                              "   abs(daughter(0).pdgId())==13||abs(daughter(1).pdgId())==13 ) "
+                                              "& mother(0).pdgId()==5100039"),
                              filter = cms.bool(True)
                             )
