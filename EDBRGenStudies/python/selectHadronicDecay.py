89baa917781e549249375609562f6031c561556a
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-06-29 18:16:22 -0300

    Include pdgId of BulkGrav

--- EDBRGenStudies/python/selectHadronicDecay.py
+++ EDBRGenStudies/python/selectHadronicDecay.py
@@ -4,6 +4,6 @@ hadronicDecay = cms.EDFilter("CandViewSelector",
                              src = cms.InputTag("prunedGenParticles"),
                              cut = cms.string(" 22 < abs(pdgId()) < 26 "
                                               "& 0 < abs(daughter(0).pdgId()) < 6 "
-                                              "& mother(0).pdgId()==5100039"),
+                                              "& (mother(0).pdgId()==39||mother(0).pdgId()==5100039)"),
                              filter = cms.bool(True)
                             )
