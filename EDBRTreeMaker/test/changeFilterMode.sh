485ba690d588b095572310025f90fef7835a1463
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-07-21 10:41:43 -0300

    Script to change the filter mode

---
+++ EDBRTreeMaker/test/changeFilterMode.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#
+# filterMode == True 
+#                     * Filter events before the EDBRTreeMaker 
+#                     * Endpath with the trigReportAnalyser is included
+#
+# filterMode == False
+#                     * Do not filter events before the EDBRTreeMaker
+#                     * Endpath is removed
+#                     * hltSequence is removed.
+#                       Use this mode for trigger studies
+#
+# Change filter mode from True to False with the following command:
+sed -i '208 s/True/False/' *.py
