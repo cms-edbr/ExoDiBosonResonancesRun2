b81953f6c8c7a8c5ac319536158b8dae3e2fe9fa
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-07-23 16:38:01 -0300

    Fix in TrigReportData

--- EDBRCommon/plugins/TrigReportData.cc
+++ EDBRCommon/plugins/TrigReportData.cc
@@ -62,8 +62,8 @@ void TrigReportData::analyze(const edm::Event& iEvent, const edm::EventSetup& iS
       case  0: cutFlow->Fill("Begin",1); break;
       case  3: cutFlow->Fill("Begin",1); cutFlow->Fill("HLT",1); break;
       case 10: cutFlow->Fill("Begin",1); cutFlow->Fill("HLT",1); cutFlow->Fill("Vertex",1); break;
-      case 15: cutFlow->Fill("Begin",1); cutFlow->Fill("HLT",1); cutFlow->Fill("Vertex",1); cutFlow->Fill("Leptons",1); break;
-      case 16: cutFlow->Fill("Begin",1); cutFlow->Fill("HLT",1); cutFlow->Fill("Vertex",1); cutFlow->Fill("Leptons",1); cutFlow->Fill("Dilepton",1); break;
+      case 11: cutFlow->Fill("Begin",1); cutFlow->Fill("HLT",1); cutFlow->Fill("Vertex",1); cutFlow->Fill("Leptons",1); break;
+      case 15: cutFlow->Fill("Begin",1); cutFlow->Fill("HLT",1); cutFlow->Fill("Vertex",1); cutFlow->Fill("Leptons",1); cutFlow->Fill("Dilepton",1); break;
       case 17: cutFlow->Fill("Begin",1); cutFlow->Fill("HLT",1); cutFlow->Fill("Vertex",1); cutFlow->Fill("Leptons",1); cutFlow->Fill("Dilepton",1); cutFlow->Fill("V-jet",1); break;
       case 18: cutFlow->Fill("Begin",1); cutFlow->Fill("HLT",1); cutFlow->Fill("Vertex",1); cutFlow->Fill("Leptons",1); cutFlow->Fill("Dilepton",1); cutFlow->Fill("V-jet",1); cutFlow->Fill("Graviton",1); break;
   }
