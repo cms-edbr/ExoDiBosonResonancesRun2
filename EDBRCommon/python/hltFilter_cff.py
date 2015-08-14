131da04c441bf0e430113a849c68f88242ae1fcf
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-08-13 23:45:19 -0300

    Producer for HLT matching

--- EDBRCommon/python/hltFilter_cff.py
+++ EDBRCommon/python/hltFilter_cff.py
@@ -4,9 +4,26 @@ import HLTrigger.HLTfilters.triggerResultsFilter_cfi as hlt
 
 hltFilter = hlt.triggerResultsFilter.clone(
                 triggerConditions = ( 'HLT_Ele105_CaloIdVT_GsfTrkIdT_v*', 'HLT_Mu45_eta2p1_v*' ),
-                #triggerConditions = ( 'HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_MW_v*', 'HLT_Mu30_TkMu11_v*' ),
                 hltResults = cms.InputTag( "TriggerResults", "", "HLT" ),
                 l1tResults = cms.InputTag( "gtDigis" ),
                 throw = False )
 
-hltSequence = cms.Sequence( hltFilter )
+hltMatchingElectrons = cms.EDProducer("PatElectronHLTmatching",
+                                  path       = cms.vstring ("HLT_Ele105_CaloIdVT_GsfTrkIdT_v*" ), 
+                                  hltToken   = cms.InputTag("TriggerResults","","HLT"          ),
+                                  hltObjects = cms.InputTag("selectedPatTrigger"               ),
+                                  leptons    = cms.InputTag("slimmedElectrons"                 ),
+                                  maxDeltaR  = cms.double  ( 0.3                               ),
+                                  maxDeltaPt = cms.double  ( 0.1                               ))
+
+hltMatchingMuons = cms.EDProducer("PatMuonHLTmatching",
+                                  path       = cms.vstring ("HLT_Mu45_eta2p1_v*"               ),
+                                  hltToken   = cms.InputTag("TriggerResults","","HLT"          ),
+                                  hltObjects = cms.InputTag("selectedPatTrigger"               ),
+                                  leptons    = cms.InputTag("slimmedMuons"                     ),
+                                  maxDeltaR  = cms.double  ( 0.3                               ),
+                                  maxDeltaPt = cms.double  ( 0.1                               ))
+
+hltSequence = cms.Sequence( hltFilter            + 
+                            hltMatchingElectrons +
+                            hltMatchingMuons     )
