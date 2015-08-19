cd5366f0d923fe7ce116121c9889cc8d445e009d
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-08-19 17:58:08 -0300

    Introduce module to pick the best hadronicV candidate

--- EDBRCommon/python/hadronicZ_cff.py
+++ EDBRCommon/python/hadronicZ_cff.py
@@ -1,8 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-hadronicV = cms.EDFilter("PATJetSelector",
-                         src = cms.InputTag("cleanJets"),
-                         cut = cms.string("70. < mass < 110.")
-                         )
+hadronicV = cms.EDFilter( "CandViewSelector",
+                          src = cms.InputTag("cleanJets"),
+                          cut = cms.string('pt > 200. '
+                                           '& (userFloat("ak8PFJetsCHSSoftDropMass") > 50.) '
+                                           '& (userFloat("ak8PFJetsCHSSoftDropMass") < 110.)'),
+                          filter = cms.bool(True) )
 
 hadronicVSequence = cms.Sequence(hadronicV)
