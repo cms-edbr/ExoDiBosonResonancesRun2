04db1c9ce02d4249facc4b5526a0d8d5bd70d417
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-08-23 00:22:32 +0200

    Correct luminosity and JSON file

--- EDBRTreeMaker/data/analysis-SingleElectron_Run2015B.py
+++ EDBRTreeMaker/data/analysis-SingleElectron_Run2015B.py
@@ -45,7 +45,7 @@ usedNevents = configNevents[SAMPLE]
 # https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/
 # last modified 21-Aug-2015 
 import FWCore.PythonUtilities.LumiList as LumiList
-process.source.lumisToProcess = LumiList.LumiList(filename = 'Cert_246908-254349_13TeV_PromptReco_Collisions15_JSON.txt').getVLuminosityBlockRange()
+process.source.lumisToProcess = LumiList.LumiList(filename = 'Cert_246908-251883_13TeV_PromptReco_Collisions15_JSON.txt').getVLuminosityBlockRange()
 
 #*******************************************************************************************************#
 
@@ -96,7 +96,7 @@ process.treeDumper = cms.EDAnalyzer(      "EDBRTreeMaker",
                                           isGen           = cms.bool    (  False                     ),
                                           originalNEvents = cms.int32   (  usedNevents               ),
                                           crossSectionPb  = cms.double  (  usedXsec                  ),
-                                          targetLumiInvPb = cms.double  (  41.202                    ),
+                                          targetLumiInvPb = cms.double  (  40.863                    ),
                                           EDBRChannel     = cms.string  (  CHANNEL                   ),
                                           gravitonSrc     = cms.string  ( "graviton"                 ),
                                           metSrc          = cms.string  ( "slimmedMETs"              ),
