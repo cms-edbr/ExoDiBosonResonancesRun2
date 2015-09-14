102ad0d613c3a1f2c667da576c43b08a7750c561
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-09-14 04:10:18 +0200

    Redistribute files

--- EDBRCommon/python/simulation/RunIIDR74X/DYJetsToLL/HT-200to400-ac.py
+++ EDBRCommon/python/simulation/RunIIDR74X/DYJetsToLL/HT-200to400-ac.py
@@ -5,6 +5,9 @@ readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring()
 source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( [
+       '/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v2/50000/707573A8-732B-E511-A9F2-001E67A40451.root',
+       '/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v2/50000/72EB38A2-5A2B-E511-B218-00261894388A.root',
+       '/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v2/50000/AE591DF1-B632-E511-98C8-002590D8C800.root', 
        '/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v2/50000/C00DED59-872C-E511-A3EE-001EC9B21623.root',
        '/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v2/50000/C0932BAA-732B-E511-8437-90B11C050371.root',
        '/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v2/50000/D8AC7B96-622E-E511-BD9E-001E4F339C72.root',
