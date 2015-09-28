70a836e14912a70f6634b2475030b6a827f6cf6e
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-09-28 19:37:01 +0200

    Moving to CMSSW_7_4_13

--- EDBRTreeMaker/data/Run2015B/submitJob.sh
+++ EDBRTreeMaker/data/Run2015B/submitJob.sh
@@ -14,7 +14,7 @@
 #            bsub -q 8nh -M 2000 -J SingleE < submitJob.sh
 
 export X509_USER_PROXY=/afs/cern.ch/user/j/jruizvar/x509up_u9472
-cd /afs/cern.ch/work/j/jruizvar/private/Fall15/25ns/CMSSW_7_4_12_patch4/src/ExoDiBosonResonances/EDBRTreeMaker/data/Run2015B
+cd /afs/cern.ch/work/j/jruizvar/private/Fall15/25ns/CMSSW_7_4_13/src/ExoDiBosonResonances/EDBRTreeMaker/data/Run2015B
 eval `scramv1 runtime -sh`
 
 cmsRun analysis-SingleElectron.py
