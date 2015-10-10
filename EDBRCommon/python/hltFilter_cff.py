be0c2f40763e4a20da5c491588fac7ec968fb825
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-10-10 08:56:28 +0200

    Set trigger condition for electron channel

--- EDBRCommon/python/hltFilter_cff.py
+++ EDBRCommon/python/hltFilter_cff.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 import HLTrigger.HLTfilters.triggerResultsFilter_cfi as hlt
 
 hltFilter = hlt.triggerResultsFilter.clone(
-                                  triggerConditions =      ( 'HLT_Ele105_CaloIdVT_GsfTrkIdT_v*', 'HLT_Mu45_eta2p1_v*' ),
+                                  triggerConditions =      ( 'HLT_Ele105_CaloIdVT_GsfTrkIdT_v*', ),
+                                  #triggerConditions =      ( 'HLT_Mu45_eta2p1_v*',             ),
                                   hltResults = cms.InputTag( "TriggerResults", "", "HLT"       ),
                                   l1tResults = cms.InputTag( "gtDigis"                         ),
                                   throw = False )
