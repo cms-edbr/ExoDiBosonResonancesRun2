7a3ddab7b3d1290e9855d65088df58d190f01da1
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-10-11 20:11:33 +0200

    Split DYJet HT200

--- EDBRTreeMaker/backgrounds/DYJets/submitAll.sh
+++ EDBRTreeMaker/backgrounds/DYJets/submitAll.sh
@@ -12,7 +12,10 @@ sed -i '20 s/100to200-ae/100to200-af/' submitJob.sh; bsub -q 8nh -M 2000 -J DY10
 sed -i '20 s/100to200-af/200to400-aa/' submitJob.sh; bsub -q 8nh -M 2000 -J DY200aa < submitJob.sh
 sed -i '20 s/200to400-aa/200to400-ab/' submitJob.sh; bsub -q 8nh -M 2000 -J DY200ab < submitJob.sh
 sed -i '20 s/200to400-ab/200to400-ac/' submitJob.sh; bsub -q 8nh -M 2000 -J DY200ac < submitJob.sh
-sed -i '20 s/200to400-ac/400to600-aa/' submitJob.sh; bsub -q 8nh -M 2000 -J DY400aa < submitJob.sh
+sed -i '20 s/200to400-ac/200to400-ad/' submitJob.sh; bsub -q 8nh -M 2000 -J DY200ad < submitJob.sh
+sed -i '20 s/200to400-ad/200to400-ae/' submitJob.sh; bsub -q 8nh -M 2000 -J DY200ae < submitJob.sh
+sed -i '20 s/200to400-ae/200to400-af/' submitJob.sh; bsub -q 8nh -M 2000 -J DY200af < submitJob.sh
+sed -i '20 s/200to400-af/400to600-aa/' submitJob.sh; bsub -q 8nh -M 2000 -J DY400aa < submitJob.sh
 sed -i '20 s/400to600-aa/400to600-ab/' submitJob.sh; bsub -q 8nh -M 2000 -J DY400ab < submitJob.sh
 sed -i '20 s/400to600-ab/400to600-ac/' submitJob.sh; bsub -q 8nh -M 2000 -J DY400ac < submitJob.sh
 sed -i '20 s/400to600-ac/400to600-ad/' submitJob.sh; bsub -q 8nh -M 2000 -J DY400ad < submitJob.sh
