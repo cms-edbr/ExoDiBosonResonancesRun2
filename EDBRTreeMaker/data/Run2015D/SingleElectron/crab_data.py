a10f93707674fe18396d91c96acbe9790a0cc8a3
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-10-17 23:16:51 +0200

    Config files for CRAB3

---
+++ EDBRTreeMaker/data/Run2015D/SingleElectron/crab_data.py
@@ -0,0 +1,26 @@
+from WMCore.Configuration import Configuration
+    
+config = Configuration()
+config.section_('General')
+config.General.transferOutputs = True
+config.General.workArea = 'data'
+config.General.transferLogs=True
+
+config.section_('JobType')
+config.JobType.psetName = 'analysis-SingleElectron.py'
+config.JobType.pluginName = 'Analysis'
+config.JobType.outputFiles = ['treeEDBR_SingleElectron.root']
+
+config.section_('Data')
+#config.Data.inputDataset= '/SingleElectron/Run2015D-PromptReco-v3/MINIAOD'
+config.Data.inputDataset= '/SingleElectron/Run2015D-PromptReco-v4/MINIAOD'
+config.Data.unitsPerJob = 200
+config.Data.splitting = 'LumiBased'
+config.Data.lumiMask = "https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-258714_13TeV_PromptReco_Collisions15_25ns_JSON.txt"
+config.Data.publication = False
+config.Data.useParent = False
+config.Data.outLFNDirBase = '/store/user/jruizvar/data/2015/EDBRtrees'
+
+config.section_('User')
+config.section_('Site')
+config.Site.storageSite = 'T2_BR_SPRACE'
