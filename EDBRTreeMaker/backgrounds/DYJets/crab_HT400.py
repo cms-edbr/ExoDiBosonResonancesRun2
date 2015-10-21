334d8e25780699d2c6967c03cdd983c4374e3e6a
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-10-21 07:39:38 +0200

    Align parameters

--- EDBRTreeMaker/backgrounds/DYJets/crab_HT400.py
+++ EDBRTreeMaker/backgrounds/DYJets/crab_HT400.py
@@ -1,13 +1,13 @@
 from CRABClient.UserUtilities import config
 config = config()
     
-config.JobType.pluginName      = 'Analysis'
-config.JobType.psetName        = 'analysis-DYJetsToLL.py'
-config.JobType.pyCfgParams     = ['','HT-400to600','mu']
+config.JobType.pluginName  = 'Analysis'
+config.JobType.psetName    = 'analysis-DYJetsToLL.py'
+config.JobType.pyCfgParams = ['','HT-400to600','el']
 
-config.Data.inputDataset  = '/DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v2/MINIAODSIM'
-config.Data.unitsPerJob   = 5
-config.Data.splitting     = 'FileBased'
-config.Data.outLFNDirBase = '/store/user/jruizvar/mc/2015/EDBRtrees'
+config.Data.inputDataset   = '/DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v2/MINIAODSIM'
+config.Data.unitsPerJob    = 5
+config.Data.splitting      = 'FileBased'
+config.Data.outLFNDirBase  = '/store/user/jruizvar/mc/2015/EDBRtrees/el'
 
-config.Site.storageSite = 'T2_BR_SPRACE'
+config.Site.storageSite    = 'T2_BR_SPRACE'
--- EDBRTreeMaker/backgrounds/TTbar/crab_TT.py
+++ EDBRTreeMaker/backgrounds/TTbar/crab_TT.py
@@ -1,13 +1,13 @@
 from CRABClient.UserUtilities import config
 config = config()
     
-config.JobType.pluginName      = 'Analysis'
-config.JobType.psetName        = 'analysis-TT.py'
-config.JobType.pyCfgParams     = ['','mu']
+config.JobType.pluginName  = 'Analysis'
+config.JobType.psetName    = 'analysis-TT.py'
+config.JobType.pyCfgParams = ['','el']
 
-config.Data.inputDataset  = '/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM'
-config.Data.unitsPerJob   = 5
-config.Data.splitting     = 'FileBased'
-config.Data.outLFNDirBase = '/store/user/jruizvar/mc/2015/EDBRtrees'
+config.Data.inputDataset   = '/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM'
+config.Data.unitsPerJob    = 5
+config.Data.splitting      = 'FileBased'
+config.Data.outLFNDirBase  = '/store/user/jruizvar/mc/2015/EDBRtrees/el'
 
-config.Site.storageSite = 'T2_BR_SPRACE'
+config.Site.storageSite    = 'T2_BR_SPRACE'
