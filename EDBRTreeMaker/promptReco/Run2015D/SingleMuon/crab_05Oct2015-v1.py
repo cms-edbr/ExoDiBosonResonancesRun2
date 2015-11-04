cc09b4ff6d8d4b117b0ac52857a0cc549d1b105f
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-11-04 17:32:20 -0200

    Update crab configs

--- EDBRTreeMaker/promptReco/Run2015D/SingleMuon/crab_05Oct2015-v1.py
+++ EDBRTreeMaker/promptReco/Run2015D/SingleMuon/crab_05Oct2015-v1.py
@@ -8,10 +8,11 @@ config.section_('JobType')
 config.JobType.psetName = 'analysis-SingleMuon.py'
 config.JobType.pluginName = 'Analysis'
 config.JobType.outputFiles = ['treeEDBR_SingleMuon.root']
+config.JobType.maxJobRuntimeMin = 180
 
 config.section_('Data')
 config.Data.inputDataset= '/SingleMuon/Run2015D-05Oct2015-v1/MINIAOD'
-config.Data.unitsPerJob = 200
+config.Data.unitsPerJob = 150
 config.Data.splitting = 'LumiBased'
 config.Data.lumiMask = "https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-259891_13TeV_PromptReco_Collisions15_25ns_JSON.txt"
 config.Data.publication = False
@@ -21,4 +22,3 @@ config.Data.outLFNDirBase = '/store/user/jruizvar/data/2015/EDBRtrees'
 config.section_('User')
 config.section_('Site')
 config.Site.storageSite =  'T2_BR_SPRACE'
-config.Site.whitelist   = ['T2_BR_SPRACE']
