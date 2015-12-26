8b3c7147351d61d542349c6d5a5c4eb7e5748be6
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-12-26 18:58:08 +0100

    Script to submit crab jobs

--- EDBRTreeMaker/backgrounds/crabConfig.py
+++ EDBRTreeMaker/backgrounds/crabConfig.py
@@ -1,15 +1,16 @@
 from CRABClient.UserUtilities import config
 config = config()
-    
+
 config.JobType.pluginName  = 'Analysis'
 config.JobType.psetName    = 'analysis-background.py'
-config.JobType.pyCfgParams = ['','HT-600toInf','mu']
+config.JobType.pyCfgParams = ['','TT','mu']
 config.JobType.maxJobRuntimeMin = 120
 
-config.Data.inputDataset   = '/DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM'
-config.Data.unitsPerJob    = 1
+config.Data.inputDataset   = '/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM'
+config.Data.unitsPerJob    = 3
 config.Data.splitting      = 'FileBased'
 config.Data.outLFNDirBase  = '/store/user/jruizvar/mc/2015/EDBRtrees/mu'
+config.Data.publication    = False
 
 config.Site.storageSite    =  'T2_BR_SPRACE'
 config.Site.whitelist      = ['T2_BR_SPRACE']
