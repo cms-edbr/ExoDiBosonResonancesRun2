8e9908b327acdbd7bc39ae13e0eb92fc35c5d8ba
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-12-29 16:25:52 +0100

    Config files for crab

--- EDBRTreeMaker/backgrounds/crabConfig.py
+++ EDBRTreeMaker/backgrounds/crabConfig.py
@@ -6,7 +6,7 @@ config.JobType.psetName    = 'analysis-background.py'
 config.JobType.pyCfgParams = ['','TT','mu']
 config.JobType.maxJobRuntimeMin = 120
 
-config.Data.inputDataset   = '/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM'
+config.Data.inputDataset   = '/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIIFall15MiniAODv1-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM'
 config.Data.unitsPerJob    = 3
 config.Data.splitting      = 'FileBased'
 config.Data.outLFNDirBase  = '/store/user/jruizvar/mc/2015/EDBRtrees/mu'
