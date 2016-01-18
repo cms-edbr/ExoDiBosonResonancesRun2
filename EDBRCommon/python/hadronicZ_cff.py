29beabd0f924d9ca13aa43b01b6a295fa6d6166a
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2016-01-18 01:39:41 +0100

    Include nsubjettiness cut in baseline selection

--- EDBRCommon/python/hadronicZ_cff.py
+++ EDBRCommon/python/hadronicZ_cff.py
@@ -7,9 +7,16 @@ corrJetsProducer = cms.EDProducer ( "CorrJetsProducer",
                                     payload = cms.string  ( "AK8PFchs"                 ),
                                     isData  = cms.bool    (  False                     ))
 
-hadronicV = cms.EDFilter( "CandViewSelector",
-                          src = cms.InputTag("corrJetsProducer:corrJets"),
-                          cut = cms.string('abs(eta)<2.4'),
-                          filter = cms.bool(True) )
+nsubjettiness = cms.EDFilter(      "CandViewSelector",
+                                    src = cms.InputTag("corrJetsProducer:corrJets"),
+                                    cut = cms.string('(userFloat("NjettinessAK8:tau2")/userFloat("NjettinessAK8:tau1"))<0.75'),
+                                    filter = cms.bool(True) )
 
-hadronicVSequence = cms.Sequence( corrJetsProducer + hadronicV )
+hadronicV = cms.EDFilter(          "CandViewSelector",
+                                    src = cms.InputTag("nsubjettiness"),
+                                    cut = cms.string('abs(eta)<2.4'),
+                                    filter = cms.bool(True) )
+
+hadronicVSequence = cms.Sequence(   corrJetsProducer + 
+                                    nsubjettiness    +
+                                    hadronicV        )
